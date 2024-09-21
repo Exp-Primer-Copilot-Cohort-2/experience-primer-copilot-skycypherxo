ff5bb0af33068e67ade54b3537fa7730942b5ce6
Skycyph3r XO <89746236+skycypherxo@users.noreply.github.com>
2024-09-21 16:32:43 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,40 +1,14 @@
-//Create a server
+//Create web server
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-const fs = require('fs');
-const path = require('path');
+const port = 3000;
 
-app.use(bodyParser.json());
-
-app.get('/comments', (req, res) => {
-    fs.readFile(path.join(__dirname, 'comments.json'), 'utf8', (err, data) => {
-        if (err) {
-            res.status(500).send('Error reading file');
-            return;
-        }
-        res.send(JSON.parse(data));
-    });
+//Create a route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-app.post('/comments', (req, res) => {
-    fs.readFile(path.join(__dirname, 'comments.json'), 'utf8', (err, data) => {
-        if (err) {
-            res.status(500).send('Error reading file');
-            return;
-        }
-        const comments = JSON.parse(data);
-        comments.push(req.body);
-        fs.writeFile(path.join(__dirname, 'comments.json'), JSON.stringify(comments), 'utf8', (err) => {
-            if (err) {
-                res.status(500).send('Error writing file');
-                return;
-            }
-            res.send('Comment added');
-        });
-    });
-});
-
-app.listen(3000, () => {
-    console.log('Server listening on port 3000');
+//Start server
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
